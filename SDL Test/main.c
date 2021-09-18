54424b69bab5e0e14a2d3a0826ac577818485cb1
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-18 13:56:19 +0200

    Initial Commit

---
+++ SDL Test/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SDL Test
+//
+//  Created by Aurelien Borst on 9/18/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
